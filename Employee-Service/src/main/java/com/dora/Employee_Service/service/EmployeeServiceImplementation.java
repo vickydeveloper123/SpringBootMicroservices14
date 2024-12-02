02c45fa60f32d489cb0a30e99629416803119c40
doraashokit24gmail.com <dorasrivignesh@gmail.com>
2024-12-02 15:23:07 +0530

    Adding web client1  microservices project

--- Employee-Service/src/main/java/com/dora/Employee_Service/service/EmployeeServiceImplementation.java
+++ Employee-Service/src/main/java/com/dora/Employee_Service/service/EmployeeServiceImplementation.java
@@ -9,18 +9,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
 public class EmployeeServiceImplementation implements EmployeeService {
 
     @Autowired
     private EmployeeRepository employeeRepository;
+@Autowired
+    public WebClient webClient;
 
-    public RestTemplate restTemplate;
 
-    public EmployeeServiceImplementation(EmployeeRepository employeeRepository) {
-        this.employeeRepository = employeeRepository;
-    }
     EmployeeServiceImplementation(){
         System.out.println("Employee Service Implementation got sucessfully created....");
     }
@@ -50,14 +49,19 @@ public class EmployeeServiceImplementation implements EmployeeService {
     @Override
     public APIResponseDTO getEmployeeDetails(Long employeeId) {
          Employee employeeDto= employeeRepository.findById(employeeId).get();
-        ResponseEntity<DepartmentDto> responseEntity= restTemplate
-                .getForEntity("http://localhost:8080/api/departments/IT001"
-                        +employeeDto.getDepartmentCode(),
-                 DepartmentDto.class);
+//        ResponseEntity<DepartmentDto> responseEntity= restTemplate
+//                .getForEntity("http://localhost:8080/api/departments/IT001"
+//                        +employeeDto.getDepartmentCode(),
+//                 DepartmentDto.class);
 
-        DepartmentDto departmentDto=responseEntity.getBody();
+        System.out.println(webClient);
+        DepartmentDto departmentDto=   webClient.get()
+                .uri("http://localhost:8080/api/departments/"+employeeDto.getDepartmentCode())
+                .retrieve()
+                .bodyToMono(DepartmentDto.class).block();
 
-        System.out.println(departmentDto);
+
+
 
          EmployeeDto employeeDto1=new EmployeeDto(
                  employeeDto.getId(),
@@ -68,6 +72,8 @@ public class EmployeeServiceImplementation implements EmployeeService {
          );
 
 
+
+
         APIResponseDTO apiResponseDTO=new APIResponseDTO();
         apiResponseDTO.setEmployeeDto(employeeDto1);
         apiResponseDTO.setDepartmentDto(departmentDto);
@@ -76,3 +82,81 @@ public class EmployeeServiceImplementation implements EmployeeService {
     }
 
 }
+
+
+//package com.dora.Employee_Service.service;
+//
+//import com.dora.Employee_Service.dto.APIResponseDTO;
+//import com.dora.Employee_Service.dto.DepartmentDto;
+//import com.dora.Employee_Service.dto.EmployeeDto;
+//import com.dora.Employee_Service.entity.Employee;
+//import com.dora.Employee_Service.repository.EmployeeRepository;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//import org.springframework.web.reactive.function.client.WebClient;
+//
+//import java.util.Optional;
+//
+//@Service
+//public class EmployeeServiceImplementation implements EmployeeService {
+//
+//    private final EmployeeRepository employeeRepository;
+//    private final WebClient webClient;
+//
+//    @Autowired
+//    public EmployeeServiceImplementation(EmployeeRepository employeeRepository, WebClient.Builder webClientBuilder) {
+//        this.employeeRepository = employeeRepository;
+//        this.webClient = webClientBuilder.baseUrl("http://localhost:8080/api/departments/IT001").build();
+//    }
+//
+//    @Override
+//    public EmployeeDto saveEmployeeDetails(EmployeeDto employeeDto) {
+//        Employee employee = new Employee(
+//                employeeDto.getId(),
+//                employeeDto.getFirstName(),
+//                employeeDto.getLastName(),
+//                employeeDto.getEmail(),
+//                employeeDto.getDepartmentCode()
+//        );
+//
+//        Employee savedEmployee = employeeRepository.save(employee);
+//
+//        return new EmployeeDto(
+//                savedEmployee.getId(),
+//                savedEmployee.getFirstName(),
+//                savedEmployee.getLastName(),
+//                savedEmployee.getEmail(),
+//                savedEmployee.getDepartmentCode()
+//        );
+//    }
+//
+//    @Override
+//    public APIResponseDTO getEmployeeDetails(Long employeeId) {
+//        Optional<Employee> employeeOptional = employeeRepository.findById(employeeId);
+//        if (employeeOptional.isEmpty()) {
+//            throw new RuntimeException("Employee not found with ID: " + employeeId);
+//        }
+//
+//        Employee employee = employeeOptional.get();
+//
+//        EmployeeDto employeeDto = new EmployeeDto(
+//                employee.getId(),
+//                employee.getFirstName(),
+//                employee.getLastName(),
+//                employee.getEmail(),
+//                employee.getDepartmentCode()
+//        );
+//
+//        DepartmentDto departmentDto = webClient.get()
+//                .uri("http://localhost:8080/api/departments/IT001" + employee.getDepartmentCode())
+//                .retrieve()
+//                .bodyToMono(DepartmentDto.class)
+//                .block();
+//
+//        APIResponseDTO apiResponseDTO = new APIResponseDTO();
+//        apiResponseDTO.setEmployeeDto(employeeDto);
+//        apiResponseDTO.setDepartmentDto(departmentDto);
+//
+//        return apiResponseDTO;
+//    }
+//}
